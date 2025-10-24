e1a515629c5dd9907f72303d7e6833e1fa842f46
KUMAR <kumarswamy.g2005@gmail.com>
2025-10-24 13:41:01 +0530

    Add 404 NotFound page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="text-center py-5">
+      <h1 className="display-1">404</h1>
+      <h2 className="mb-4">Page Not Found</h2>
+      <p className="lead mb-4">
+        The page you are looking for doesn&apos;t exist or has been moved.
+      </p>
+      <Link to="/" className="btn btn-primary">
+        Go to Homepage
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
